625198b8d6d969da6c72e9a715a71755c7a87bfc
TranTienDo <tiendo030204@gmail.com>
2022-10-14 14:28:58 +0700

    Add main.js file

---
+++ main.js
@@ -0,0 +1,56 @@
+/*
+Kiểu dữ liệu trong Javascript
+
+1. Dữ liệu nguyên thủy - Primittive Data
+  - Number
+  -String
+  -Boolean
+  -Underfiend
+  -Null
+  -Symbol
+ 2.Dữ liệu phức tạp - Complex Data
+  -Function
+  -Object 
+
+ */
+//Number
+  var a = 1;
+  var b = 2;
+  var c= 1.8;
+
+//String
+var fullName = ' Tie Do ';
+
+//Boolean
+var isSuccess = true;
+
+//Undefined 
+var age;
+
+//Null
+var isNull=null;//nothing
+
+//symbol
+var id = Symbol(' id ');//unique
+var id2 = Symbol(' id ');//unique
+
+//Function
+var myFunction = function()
+{
+    alert('lo lo lo')
+}
+myFunction();
+
+//Object 
+var myObject = {
+name: ' Tie do ',
+age: 18,
+adress: 'Ninh Thuan',
+myFunction: function(){
+
+}
+}; 
+
+var myArray = [
+    
+];
